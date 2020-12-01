9cf09c7943b004fc087fef09727464717af9ff77
João Vitor Sousa <joaovfsousa@gmail.com>
2020-12-01 12:20:58 -0300

    Feat: Add Health route
    Sets the route Health and its tests

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ app = flask.Flask(__name__)
 app.debug = True
 
 
-@app.route('/', methods=['GET'])
-def root():
-  return dict(message='Success')
+@app.route('/health', methods=['GET'])
+def health():
+  response = {
+    "status": 200,
+    "message": "All right with the service",
+    "code": "success",
+    "data": {
+      "environment": "development",
+      "datetime_server": "2020-11-03T14:34:58.139738",
+      "version": "1-0-2"
+    }
+  }
+  return response
 
 
-app.run(port=9095)
+if __name__ == '__main__':
+  app.run(port=9095)
---
+++ tests/test_health_route.py
@@ -0,0 +1,33 @@
+import unittest
+
+from app import app
+
+
+class HealthRouteTest(unittest.TestCase):
+
+  def setUp(self):
+    self.app = app.test_client()
+
+  def test_health_route_response(self):
+    response = self.app.get('/health')
+    expected = {
+      "status": 200,
+      "message": "All right with the service",
+      "code": "success",
+      "data": {
+        "environment": "development",
+        "datetime_server": "2020-11-03T14:34:58.139738",
+        "version": "1-0-2"
+      }
+    }
+
+    self.assertEqual(response.json, expected)
+
+  def test_health_route_status_code(self):
+    response = self.app.get('/health')
+    expected = 200
+    self.assertEqual(response.status_code, expected)
+
+
+if __name__ == "__main__":
+  unittest.main()
